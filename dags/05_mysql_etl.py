824b44619062a42bbd94df91594c0324f45b6b47
edu-kenshi <edu.kenshi@gmail.com>
2026-04-09 14:18:21 +0900

    Feat: 모듈가져오기, 기본설정 추가

--- dags/05_mysql_etl.py
+++ dags/05_mysql_etl.py
@@ -1,18 +1,35 @@
 '''
-- 함수 내부 연산의 결과에 의해 조건부로 task를 선택하여 진행 (의존성 컨트롤)
+- etl 간단하게 적용, 스마트팩토리상 온도 센서에 대한 ETL 처리, mysql 사용
 '''
 # 1. 모듈 가져오기
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 import logging
+# 추가분
+from airflow.providers.mysql.operators.mysql import MysqlOperator
+# Load 처리시 sql에 전처리된 데이터를 밀어 넣을때 사용
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+# 데이터
+import json
+import random
+import pandas as pd  # 소량의 데이터(데이터 규모)
+import os
 
-# 2. DAG 정의
+# 2. 기본설정
+# 프로젝트 내부 폴더를 데이터용으로 (~/dags/data)지정
+# task 진행간 생성되는 파일을 동기화하도록 위치 지정 -> 향후 s3(데이터 레이크)로 대체 될수 있음
+
+# 도커 내부에 생성된 컨테이너 상 워커내의 airflow 상의 지정한 데이터 위치
+DATA_PATH = '/opt/airflow/dags/data'
+os.mkdir(DATA_PATH, exist_ok=True)
+
+# 3. DAG 정의
 with DAG() as dag:
-    # 3. task 정의
+    # 4. task 정의
     task_extract    = PythonOperator()
     task_trasform   = PythonOperator()
     task_load       = PythonOperator()
 
-    # 4. 의존성 정의 -> 시나리오별 준비 
+    # 5. 의존성 정의 -> 시나리오별 준비 
     task_extract >> task_trasform >> task_load
