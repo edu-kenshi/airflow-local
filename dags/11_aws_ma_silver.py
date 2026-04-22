ac9dc7721039c56b4d1de426f339ba4b67b82439
edu-kenshi <edu.kenshi@gmail.com>
2026-04-22 11:21:53 +0900

    Feat: silver 단계에서 목표 컬럼 구성(전처리) 설정

---
+++ dags/11_aws_ma_silver.py
@@ -0,0 +1,16 @@
+'''
+- ma 에서 silver 단계 처리
+- 데이터 (flatten, 파생변수, 컬럼명변경) 전처리 수행(sql을 통해)
+    - event_id
+    - event_time => event_timestamp
+    - data.user_id
+    - data.item_id
+    - data.price
+    - data.qty
+    - (data.price * data.qty) as total_price 
+    - data.store_id
+    - source_ip
+    - user_agent
+    - dt (year-month-day)
+    - hour as hr
+'''
