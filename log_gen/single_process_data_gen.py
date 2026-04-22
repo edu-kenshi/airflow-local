4fc15be9b89d5bf012aae29ce461da8a2fc27a25
edu-kenshi <edu.kenshi@gmail.com>
2026-04-22 09:33:50 +0900

    Feat: 로그 데이터 구성간에 json.dumps 함수 제거

--- log_gen/single_process_data_gen.py
+++ log_gen/single_process_data_gen.py
@@ -56,18 +56,19 @@ def gen_data():
   current_utc_time = datetime.now(UTC).isoformat().replace("+00:00", "Z")
 
   # raw 데이터 구성
+  # struct<user_id:string,item_id:string,price:int,qty:int,store_id:string>
   raw_log = {
       "event_id"  : str(uuid.uuid4()),  # 이벤트 관리 번호(중복되지 않게 해싱)
       "event_time": current_utc_time,   # 현재시간(이벤트 발생시간)
       "source_ip" : fake.ipv4(),        # 클라이언트의 접속 IP(페이크)
       "user_agent": fake.user_agent(),  # 클라이언트의 접속 브라우저 타입(페이크)
-      "data": json.dumps({              # 상세 데이터 => dict내에 dict 구성, 객체 직렬화추가
+      "data": {              # 상세 데이터 => dict내에 dict 구성, 객체 직렬화추가
           "user_id" : f"user_{random.randint(100, 999)}", # 사용자 id (사용자별로 여러번구매)
           "item_id" : selected_item["item_id"],           # 구매 제품
           "price"   : selected_item["price"],             # 단가
           "qty"     : qty,                                # 수량
           "store_id": "store-01"                          # 매장번호(오프/온라인 포함) 
-      }),
+      },
       "ingested_at": current_utc_time   # 로그 발생 시간(event_time) 동일
   }
   return raw_log
