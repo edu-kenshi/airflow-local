5b5fb15867d74634fc456652e8f9777db52a31fb
edu-kenshi <edu.kenshi@gmail.com>
2026-04-23 10:40:52 +0900

    Feat: 환경변수까지 설정

--- ELK/basic/data_gen/local_to_opensearch.py
+++ ELK/basic/data_gen/local_to_opensearch.py
@@ -1,3 +1,21 @@
 '''
 - 로그(데이터) 발생 -> opensearch -> 직접 전송
+- pip install -q opensearch-py
+- 구동
+    python ./ELK/basic/data_gen/local_to_opensearch.py
 '''
+# 1. 모듈 가져오기
+from opensearchpy import OpenSearch
+from datetime import datetime
+import random
+import time
+
+# 2. 환경변수
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+HOST = os.getenv('OPENSEARCH_HOST')
+AUTH = (os.getenv('AUTH_NAME'), os.getenv('AUTH_PW'))
+print(HOST, AUTH)
