8b64d0e817a99177b825510e100bd36b3bcc40ae
edu-kenshi <edu.kenshi@gmail.com>
2026-04-23 10:59:23 +0900

    Feat: 인덱스 생성

--- ELK/basic/data_gen/local_to_opensearch.py
+++ ELK/basic/data_gen/local_to_opensearch.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 HOST = os.getenv('OPENSEARCH_HOST')
 AUTH = (os.getenv('AUTH_NAME'), os.getenv('AUTH_PW'))
-print(HOST, AUTH)
+#print(HOST, AUTH)
 
 # 3. AWS Opensearch 서비스 클라이언트 연결
 client = OpenSearch(
@@ -30,3 +30,10 @@ client = OpenSearch(
     ssl_assert_hostname = False,
     ssl_show_warn = False
 )
+
+# 4. 인덱스 생성 => (스마트 팩토리 -> 센서(장비)별로 구성/ )
+#    인증 오류(401) 발생하면(1회성 조치) => OpenSearch 대시보드 URL(듀얼 스택) 접속 
+#    
+index_name = 'factory-45-sensor-v1' # 공장내 45구역에 v1 센서 의미 부여함(설정)
+if not client.indices.exists(index=index_name): # 인덱스가 없는가?
+    client.indices.create(index=index_name)     # 인덱스 생성
